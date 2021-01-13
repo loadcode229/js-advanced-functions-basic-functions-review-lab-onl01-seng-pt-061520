af5052dbaa1c8f798cf0eb512dd20cafc3bf7b9c
Your Name <you@example.com>
2021-01-12 20:20:24 -0500

    commit tree ad714722ffeacf2b859824e148577f56b673a558

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Your code here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity="go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(flair="*") {
+    return function(adjective="special") {
+        return `You are ${flair}${adjective}${flair}!`;
+    }
+}
+
+const Calculator = {
+    add: function(a, b) {
+        return a + b;
+    },
+    subtract: function(a, b) {
+        return a - b;
+    },
+
+    multiply: function(a, b) {
+        return a * b;
+    },
+
+    divide: function(a, b) {
+        return a / b;
+    }
+}
+
+function actionApplyer(integer, array_of_functions) {
+    for (let i = 0; i < array_of_functions.length; i++) {
+        integer = array_of_functions[i](integer)
+    };
+    return integer;
+}
